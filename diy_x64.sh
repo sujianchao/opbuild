314481303201eda2a7d1fd1bba25e89cf47b2619
jsda <jsda@github.com>
2020-03-27 16:54:16 +0800

    20200327-1654

--- diy_x64.sh
+++ diy_x64.sh
@@ -219,6 +219,6 @@ CONFIG_PACKAGE_openssh-sftp-server=y
 EOF
 # 取消编译VMware镜像以及镜像填充 (不要删除被缩进的注释符号):
 cat >> .config <<EOF
-CONFIG_VDI_IMAGES=y
-CONFIG_VMDK_IMAGES=y
+#CONFIG_VDI_IMAGES=y
+#CONFIG_VMDK_IMAGES=y
 EOF
